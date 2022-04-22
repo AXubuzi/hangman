a3137f5809dfd9ac33c5e5207aff56adec6ef61b
AXubuzi <102726899+AXubuzi@users.noreply.github.com>
2022-04-22 15:25:47 +0200

    first commit

---
+++ HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HangmanRenderer.Renderer;
+
+namespace Hangman.Core.Game
+{
+    string[] Guessingwords = { "water, science, rock, soil, data" };
+    public class HangmanGame
+    {
+        private GallowsRenderer _renderer;
+        private string [] _Guessingwords;
+        private int _NumberOfLives;
+
+        public HangmanGame()
+        {
+            _renderer = new GallowsRenderer();
+        }
+
+        public void Run()
+        {
+            _renderer.Render(5, 5, 6);
+
+            Console.SetCursorPosition(0, 13);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Your current guess: ");
+            Console.WriteLine("--------------");
+            Console.SetCursorPosition(0, 15);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("What is your next guess: ");
+            var nextGuess = Console.ReadLine();
+        }
+
+    }
+}
